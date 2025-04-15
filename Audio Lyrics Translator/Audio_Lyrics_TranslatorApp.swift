9958c4446285c47a9c12e328847c12328b4bca6f
tastyheadphones <tastyheadphones@icloud.com>
2025-04-15 21:03:38 +0800

    init commit

---
+++ Audio Lyrics Translator/Audio_Lyrics_TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_Lyrics_TranslatorApp.swift
+//  Audio Lyrics Translator
+//
+//  Created by Young Geo on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Audio_Lyrics_TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
