b4aee4df77386a073a99bfb3d9258f7e6eba0e79
tastyheadphones <tastyheadphones@icloud.com>
2025-04-17 09:25:38 +0800

    fix translator issue

--- Audio Lyrics Translator/Lyrics/LyricsView.swift
+++ Audio Lyrics Translator/Lyrics/LyricsView.swift
@@ -22,7 +22,6 @@ import SwiftUI
             for await transcript in Recogizer.generateTranscript(musicUrl: musicUrl) {
                 await MainActor.run {
                     self.originalLyrics = transcript
-                    Translator.configuration.invalidate()
                 }
             }
         }
@@ -31,29 +30,26 @@ import SwiftUI
 
 struct LyricsView: View {
     @State private var vm: LyricsViewModel
-
+    
     var body: some View {
         List {
             if let originalLyrics = vm.originalLyrics {
                 ForEach(originalLyrics.segments, id: \.self) { segment in
                     HStack {
                         Text(segment.substring)
+                            .translationTask(Translator.configuration) { session in
+                                if vm.translatedLyricsDict[segment.substring] != nil { return }
+                                do {
+                                    let translatedText = try await session.translate(segment.substring).targetText
+                                    vm.translatedLyricsDict[segment.substring] = translatedText
+                                } catch {
+                                    print("Error translate speech: \(error)")
+                                }
+                            }
                         Spacer()
                         Text(vm.translatedLyricsDict[segment.substring] ?? "No")
                             .foregroundColor(.red)
                     }
-                }
-            }
-        }
-        .translationTask(Translator.configuration) { session in
-            let sourceBatchText = vm.originalLyrics?.segments.map { $0.substring } ?? []
-            for text in sourceBatchText {
-                if vm.translatedLyricsDict[text] != nil { return }
-                do {
-                    let translatedText = try await session.translate(text).targetText
-                    vm.translatedLyricsDict[text] = translatedText
-                } catch {
-                    print("Error translate speech: \(error)")
                 }
             }
         }
